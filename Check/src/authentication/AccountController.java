0f1bd008f807814bf91c326cf2e565f6e8f567ae
anhthanhnien <62206758+anhthanhnien@users.noreply.github.com>
2020-05-10 21:28:20 +0700

    Add files via upload

---
+++ Check/src/authentication/AccountController.java
@@ -0,0 +1,49 @@
+package authentication;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AccountController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AccountController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		synchronized(session) {
+			User user = (User) session.getAttribute("user");
+			session.setAttribute("application", BookingApplication.getApplication(user.getEmail()));
+		}
+		RequestDispatcher view = request.getRequestDispatcher("AccountView.jsp");
+		view.forward(request, response);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
