1afc2ab0fb1c9012892bc1779ea965b8153b769c
tienbuiba <57903220+tienbuiba@users.noreply.github.com>
2020-05-21 19:09:39 +0700

    commit 16

---
+++ Check/src/detail/Zoom.java
@@ -0,0 +1,43 @@
+package detail;
+
+public class Zoom {
+	private int id;
+	private String type;
+	private int bed;
+	private int price;
+	private String image;
+	
+	public Zoom(int id, String type, int bed, int price, String image) {
+		this.id = id;
+		this.type = type;
+		this.bed = bed;
+		this.price = price;
+		this.image = image;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public int getBed() {
+		return this.bed;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	public String getImage() {
+		return this.image;
+	}
+	
+	
+}
